214421f8f4cf86489a9c15054387244293063147
Duane Sibilly <duane@sibilly.com>
2018-05-10 12:50:10 -0400

    Simplify best-of and worst-of logic
    As pointed out to me by @solmsted, these modules as-forked were overly complex and could be reduced to simple one-liners. So I have done so.

--- js/transforms/bestOf.js
+++ js/transforms/bestOf.js
@@ -1,11 +1,1 @@
-export default (rolledDice, value) => {
-    const sorted = rolledDice.sort((a, b) => b - a);
-
-    return sorted.reduce((memo, item, index) => {
-        if (index < (value / 1)) {
-            memo.push(item);
-        }
-
-        return memo;
-    }, []);
-};
+export default (rolledDice, value) => rolledDice.sort((a, b) => b - a).slice(0, value);
--- js/transforms/worstOf.js
+++ js/transforms/worstOf.js
@@ -1,11 +1,1 @@
-export default (rolledDice, value) => {
-    const sorted = rolledDice.sort((a, b) => a - b);
-
-    return sorted.reduce((memo, item, index) => {
-        if (index < (value / 1)) {
-            memo.push(item);
-        }
-
-        return memo;
-    }, []);
-};
+export default (rolledDice, value) => rolledDice.sort((a, b) => a - b).slice(0, value);
